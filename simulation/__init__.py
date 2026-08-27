28aa803b82d94a2b9ffee3a36c31bb28a6b04ce0
Raatwik <218100411+Raatwik@users.noreply.github.com>
2026-08-27 20:15:20 +0530

    Fault manager added. Issue #3 dealt

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -1,4 +1,5 @@
 from simulation.engine import Simulation
+from simulation.fault_manager import FaultManager
 from simulation.lag_filter import LagFilter
 
-__all__ = ["Simulation", "LagFilter"]
+__all__ = ["Simulation", "FaultManager", "LagFilter"]
